9aafb694328d7b3a79a8e40d868e2e8676686391
koudenpa <koudenpa@hotmail.com>
2018-03-04 20:18:18 +0900

    プロジェクト ファイルを追加します。

---
+++ Rance10SaveDataSwicher/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rance10SaveDataSwicher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
